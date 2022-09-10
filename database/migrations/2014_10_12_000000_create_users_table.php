e188f30d882a435665abcb3586c9680fef41a5eb
nobuhitomurata <muratanobuhito@gmail.com>
2022-09-10 16:41:03 +0900

    テーブルを作成しました

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name',100);
-            $table->string('email')->charset("utf8")->unique();
+            $table->string('email')->unique();
             $table->tinyinteger('role');
             $table->string('password');
             $table->timestamps();
